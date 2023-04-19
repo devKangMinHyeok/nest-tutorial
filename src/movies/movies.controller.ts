d700a40f6243fdddca5a55b0c4dd884ca36defe1
Kang <rkdalsgur032@unist.ac.kr>
2023-04-19 10:52:39 +0900

    Body

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('movies') // router 역할
 export class MoviesController {
@@ -7,14 +16,19 @@ export class MoviesController {
     return 'this will return all movies';
   }
 
+  @Get('search')
+  search(@Query('year') searchingYear: string) {
+    return `We are searching for a movie made after ${searchingYear}`;
+  }
+
   @Get('/:id')
   getOne(@Param('id') movieId: string) {
     return `this will return one movie id : ${movieId}`;
   }
 
   @Post()
-  create() {
-    return 'this will create a movie';
+  create(@Body() movieData) {
+    return movieData;
   }
 
   @Delete('/:id')
@@ -22,8 +36,11 @@ export class MoviesController {
     return `this will remove a movie id : ${movieId}`;
   }
 
-  @Patch()
-  patch(@Param('id') movieId: string) {
-    return `this will patch a movie id : ${movieId}`;
+  @Patch('/:id')
+  patch(@Param('id') movieId: string, @Body() updateData) {
+    return {
+      updatedMovie: movieId,
+      ...updateData,
+    };
   }
 }
